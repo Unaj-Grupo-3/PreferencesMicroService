ff3dfdcbd4528ba091be234cf4b503fa3a24684b
maximiliano_hermosilla@hotmail.com <maximiliano_hermosilla@hotmail.com>
2023-07-29 17:41:00 -0300

    Unit Test Interest Category. Remove Docker Compose

---
+++ UnitTest/UnitTestInterestCategory.cs
@@ -0,0 +1,99 @@
+﻿using Xunit;
+using Moq;
+using FluentAssertions;
+
+using Application.UseCases;
+using Application.Interfaces;
+using Application.Models;
+using Domain.Entities;
+
+namespace UnitTest
+{
+    public class UnitTestInterestCategory
+    {
+        [Fact]
+        public async void GetAll()
+        {
+            //Arrange
+            var mockCommand = new Mock<IInterestCategoryCommand>();
+            var mockQuery = new Mock<IInterestCategoryQuery>();
+            InterestCategoryService service = new(mockQuery.Object, mockCommand.Object);
+
+            //Act
+            var lista = await service.GetAll();
+
+            //Assert
+            Assert.True(!lista.Any());
+        }
+
+        [Fact]
+        public async void Insert_Ok()
+        {
+            //Arrange
+            var mockCommand = new Mock<IInterestCategoryCommand>();
+            var mockQuery = new Mock<IInterestCategoryQuery>();
+            var request = new InterestCategoryReq { Description = "CategoryTest" };
+            InterestCategoryService service = new(mockQuery.Object, mockCommand.Object);
+
+            //Act
+            var response =  await service.Insert(request);
+
+            //Assert
+            response.Id.Should().Be(0);
+            response.Description.Should().Be(request.Description);
+        }
+
+        [Fact]
+        public async void Insert_ThrowsException()
+        {
+            //Arrange
+            var mockCommand = new Mock<IInterestCategoryCommand>();
+            var mockQuery = new Mock<IInterestCategoryQuery>();
+            var request = new InterestCategoryReq { Description = "CategoryTest" };
+            InterestCategoryService service = new(mockQuery.Object, mockCommand.Object);
+            mockCommand.Setup(q => q.Insert(It.IsAny <InterestCategory>())).ThrowsAsync(new Exception("Error al insertar"));
+
+            //Act
+            //Assert
+            await Assert.ThrowsAsync<Exception>(() => service.Insert(request));
+        }
+
+        [Fact]
+        public async void Update_Null()
+        {
+            //Arrange
+            var mockCommand = new Mock<IInterestCategoryCommand>();
+            var mockQuery = new Mock<IInterestCategoryQuery>();
+            var request = new InterestCategoryReq { Description = "CategoryTest" };
+            InterestCategoryService service = new(mockQuery.Object, mockCommand.Object);
+
+            //Act
+            var response = await service.Update(request, 1);
+
+            //Assert
+            Assert.Null(response);
+        }
+
+        [Fact]
+        public async void Update_Ok()
+        {
+            //Arrange
+            var mockCommand = new Mock<IInterestCategoryCommand>();
+            var mockQuery = new Mock<IInterestCategoryQuery>();
+            var request = new InterestCategoryReq { Description = "CategoryTest" };
+            int interestCategoryId = 1;
+            InterestCategoryService service = new(mockQuery.Object, mockCommand.Object);
+
+            mockQuery.Setup(q => q.GetById(It.IsAny<int>())).
+                                    ReturnsAsync(new InterestCategory 
+                                    { InterestCategoryId = interestCategoryId, Description = request.Description, Interests = null });
+
+            //Act
+            var response = await service.Update(request, 1);
+
+            //Assert
+            response.Id.Should().Be(interestCategoryId);
+            response.Description.Should().Be(request.Description);
+        }
+    }
+}
